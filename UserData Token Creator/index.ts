a007b87faa56a088122281ddbebd868721cc2857
SirH <micahbenac52@gmail.com>
2022-01-23 00:26:05 -0500

    go back to sha1

--- UserData Token Creator/index.ts
+++ UserData Token Creator/index.ts
@@ -5,7 +5,7 @@ import { createHmac } from 'crypto';
 
 readFile('./input.txt', 'utf8', async (err, data) => {
     const [playerId, playerToken] = data.split(' ');
-    const token = createHmac('sha256', process.env.USERDATA_AUTH).update(playerId).update(playerToken).digest('hex');
+    const token = createHmac('sha1', process.env.USERDATA_AUTH).update(playerId).update(playerToken).digest('hex');
     try {
         await writeFile('./output.txt', token);
     } catch (err) { console.log(err) }
